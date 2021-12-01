8b0a1f12b7a8d6e87e927d8577a2f5ca40b12759
Anastasia <ana.enyioma@hotmail.com>
2021-12-01 19:14:56 +0000

    task-sesson-2

--- session-2/index.js
+++ session-2/index.js
@@ -1,2 +1,10 @@
 // This is a comment in JavaScript
 // You can see where this file is connected to your HTML in the index.html on line 14
+
+console.log("Hello Everyone")
+
+console.log("Hey," + "how are you?")
+
+console.log(document.querySelector('.about').innerHTML)
+
+console.log(document.querySelectorAll('.about'))
