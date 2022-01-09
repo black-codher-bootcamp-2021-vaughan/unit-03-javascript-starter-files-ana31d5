274a570728dcf24a3d30b2298bfaae936920318f
Anastasia <ana.enyioma@hotmail.com>
2022-01-09 23:12:28 +0000

    homework

--- session-4/index.js
+++ session-4/index.js
@@ -4,28 +4,39 @@ let personA = {
     name: 'Alissa',
     likes: 'PS4',
     location: 'France',
-    Age: '36'
-}
+    age: '36'
+};
 
+let personB = {
+    name: 'Trey',
+    likes: 'sleeping',
+    location: 'Mars',
+    age: '70'
+};
 
-personA.name = 'Alissa';
-personA.likes = 'PS4';
-personA.location = 'France';
-personA.age = '36';
+let personC = {
+    name: 'Chicken',
+    likes: 'feet',
+    location: 'farm',
+    age: '0'
+};
 
+let personD = {
+    name: 'Apple',
+    likes: 'money',
+    location: 'China',
+    age: '200'
+}; 
 
+function biography(person) {
+    return person.name + ' is ' + person.age + ' years old. They live in ' + person.location + ' and they like ' + person.likes; 
 
+};
 
-
-
-
-
-
-
-
-
-
-
+console.log(biography(personA));
+console.log(biography(personB));
+console.log(biography(personC));
+console.log(biography(personD))
 
 // ****************************************************TASK 2 - MATHS OPERATORS****************************************************
 // ***Uncomment the code below to begin the task***
@@ -53,7 +64,7 @@ console.log("division: x + y " + division);
 console.log("multiplication: x * x " + multiplication);
 
 
-console.log("Addition: x + x + y " + addition);
+console.log("addition: x + x + y " + addition);
 
 
 console.log("multiplication, division: (x * y) / x " + multiplication, division)  
@@ -73,11 +84,40 @@ const IsdrivingAge = myAge > drivingAge;
 console.log('Im I old enough to drive?' + IsdrivingAge)
 
 
+// ****************************************************HOMEWORK****************************************************
 
 
+function calculator(numberA, numberB) {
+    let result = numberA + numberB
+    result = numberA + numberB
+    result = numberA * numberB
+    result = numberA / numberB
+    return result
+}
 
+ console.log(calculator(3,11));
 
+ const PersonX = {
+     name: 'Lucy',
+     age : 100,
+ }
 
+ const PersonY = {
+     name: 'Chichi',
+     age : 2,
+ }
+
+ function WhoIsOlder(x,y) {
+     const personXisOlder = x.age > y.age;
+     const resultPersonXisOlder = x.name + "is older than " + y.name + " by " + (x.age - y.age) + "years";
+     const resultPersonYisOlder = y.name + "is older than " + x.name + " by " + (y.age - x.age) + "years";
+     if (personXisOlder === true) return resultPersonXisOlder;
+     return resultPersonYisOlder;
+ }
+
+console.log(WhoIsOlder (PersonX, PersonY));
+
+// homework
 
 
 
