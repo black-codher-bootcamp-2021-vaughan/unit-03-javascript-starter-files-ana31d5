1de82018c129b78c108ebb9826865b959912d19b
Anastasia Enyioma <ana.enyioma@hotmail.com>
2022-01-12 15:58:54 +0000

    while and for loop

--- session-6/index.js
+++ session-6/index.js
@@ -3,22 +3,25 @@
 
 // ****************************************************TASK 1****************************************************
 
-let counter= 1;
+// let count= 1;
+// const randomNumber = Math.round(Math.random() * 10);
 
-while (counter <=10) {
-    counter++;
-    const randomNumber = Math.round(Math.random() * 10);
-}
+// while (count <=10) {
+//     count++;   
+// console.log(randomNumber); }
 
-console.log( 'Total random number ' + randomNumber);
+
 
 // ****************************************************TASK 2****************************************************
 
+let counter =0
+
 for (counter = 1; counter <= 10; counter++) {
     const randomNumber = Math.round(Math.random() * 10);
+ console.log(randomNumber);
+   
 }
 
-console.log(randomNumber);
 
 
 
