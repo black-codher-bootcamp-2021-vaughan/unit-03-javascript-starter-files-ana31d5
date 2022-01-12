aecba5bb112cf9bbfc8be5975adc1d3d6528dd52
Anastasia Enyioma <ana.enyioma@hotmail.com>
2022-01-12 13:29:53 +0000

    task 1

--- session-5/index.js
+++ session-5/index.js
@@ -6,29 +6,29 @@ if (total > 90) {
     console.log("You get an A*");
 }
 
-if (total > 80, total <= 90){
+if (total > 80 && total <= 90){
     console.log("You get an A");
 }
 
-if (total > 70, total <= 80) {
+if (total > 70 && total <= 80) {
     console.log("You get a B");
 } 
 
-if (total > 60, total <= 70 ) {
+if (total > 60 && total <= 70 ) {
     console.log("You get a C");
 }
 
-if (total > 50, total <= 60) {
+if (total > 50 && total <= 60) {
     console.log("You get a D");
 
 }
 
-if (total > 40, total <= 50) {
+if (total > 40 && total <= 50) {
     console.log("You get a E");
 
 }
 
-if (total > 30, total <= 40) {
+if (total > 30 && total <= 40) {
     console.log("You get a F");
 
 }
@@ -39,6 +39,6 @@ if (total <30) {
 }
 }
 
-console.log(getGrade(92));
+console.log(getGrade(55));
 
 
