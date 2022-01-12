aed565166690ce7ce8614eab3b2c901d9527b6b8
Anastasia Enyioma <ana.enyioma@hotmail.com>
2022-01-12 14:07:58 +0000

    switch task 02

--- session-5/index.js
+++ session-5/index.js
@@ -2,43 +2,62 @@
 
 
 function getGrade (total) {
-if (total > 90) {
+    switch (true) 
+    {
+case total > 90:
     console.log("You get an A*");
-}
 
-if (total > 80 && total <= 90){
+break;
+
+case total > 80 && total <= 90 :
     console.log("You get an A");
-}
 
-if (total > 70 && total <= 80) {
+break;
+
+case total > 70 && total <= 80 :
     console.log("You get a B");
-} 
+ 
 
-if (total > 60 && total <= 70 ) {
+break;
+
+case total > 60 && total <= 70 :
     console.log("You get a C");
-}
 
-if (total > 50 && total <= 60) {
+
+break;
+
+case total > 50 && total <= 60 :
     console.log("You get a D");
 
-}
 
-if (total > 40 && total <= 50) {
+break;
+
+case total > 40 && total <= 50 :
     console.log("You get a E");
 
-}
 
-if (total > 30 && total <= 40) {
+
+break;
+
+case total > 30 && total <= 40 :
     console.log("You get a F");
 
-}
 
-if (total <30) {
+
+break;
+
+case total >20 && total <= 30:
     console.log("FAIL");
 
-}
+
+
+default :
+    console.log("Unmarkable");
+
+    }
+    
 }
 
-console.log(getGrade(55));
+console.log(getGrade(1)); //console log answer unmarkable 
 
 
