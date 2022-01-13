c9a5e0ba8375e1f6579be5b574e471a6c347301b
Anastasia Enyioma <ana.enyioma@hotmail.com>
2022-01-13 22:37:51 +0000

    task1 an task2

--- session-7/index.js
+++ session-7/index.js
@@ -1,23 +1,32 @@
 // ****************************************************TASK 1****************************************************
 
+// const animals = ['cat', 'dog', 'mouse', 'elephant', 'lion', 'lepard' ]; 
 
+// animals.pop();
 
-
-
-
-
-
-
-
-
+// console.log(animals);
 
 
 
 
 // ****************************************************TASK 2****************************************************
 
+// const names = ['emmanuel', 'shiree', 'ana', 'baggy', 'uba', 'nelson','michael', 'rihanna', 'toby', 'winter', 'ruby' ];
+
+// names.sort();
+
+// names.sort().reverse();
+
+// console.log(names);
 
 
+const numbers = [ 5 , 325 , 67, 100000, 150 ];
+
+// numbers.sort();
+
+numbers.sort().reverse();
+
+console.log(numbers);
 
 
 
@@ -29,18 +38,18 @@
 // ****************************************************TASK 3****************************************************
 
 // Notice the backticks enclosing t'challa. Read https://www.javascripttutorial.net/es6/javascript-template-literals/ for an explanation on template literals
-const people = [
-  { name: `t'challa`, age: 37 },
-  { name: 'shuri', age: 16 },
-  { name: 'killmonger', age: 32 },
-];
+// const people = [
+//   { name: `t'challa`, age: 37 },
+//   { name: 'shuri', age: 16 },
+//   { name: 'killmonger', age: 32 },
+// ];
 
-// This is a helper function that should be used in the final task of the session.
-// You should pass this function to the sort method to sort the ages of each individiual in the people array of objects
-// Note: this helper function is using a tertiary operator instead of an if/else statement
-function compare(a, b) {
-  const personA = a.age;
-  const personB = b.age;
+// // This is a helper function that should be used in the final task of the session.
+// // You should pass this function to the sort method to sort the ages of each individiual in the people array of objects
+// // Note: this helper function is using a tertiary operator instead of an if/else statement
+// function compare(a, b) {
+//   const personA = a.age;
+//   const personB = b.age;
 
-  return personA > personB ? true : false;
-}
+//   return personA > personB ? true : false;
+// }
